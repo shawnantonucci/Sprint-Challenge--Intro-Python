0b337fcc3a32ac3ba69172c04207ed1cbdb1c9a5
shawn <shawnantonucci2018@gmail.com>
2019-05-31 08:10:20 -0700

    added base/sub classes

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,34 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class
+class Vehicle:
+    def __init__(self, name):
+        pass
+
+# Subclass
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        return super().__init__(name="Millinial Falcon")
+
+# Subclass
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        return super().__init__(name="Mustang")
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        return super().__init__(name="Yamaha")
